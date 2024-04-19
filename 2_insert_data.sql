d1fb081135f957d3d21a0f58ce6f205b62612dd3
IzemHamouchene <77283279+IzemHamouchene@users.noreply.github.com>
2024-04-19 10:03:46 +0100

    Update 2_insert_data.sql

--- 2_insert_data.sql
+++ 2_insert_data.sql
@@ -1,19 +1,20 @@
 -- Use the UniversityDB Database
-USE UniversityDB
+
+
 GO
-
 -- Insert teacher Data
-INSERT INTO teacher (FirstName,LastName,Phone,Email)
-VALUES ('sofian' , 'family1' , 123 , 's_f3@gmail.com')  ,  ('yacin' , 'family2' , 456 , 'y_ f4@gmail.com');
 
+
+
+  
 -- Insert student Data
-INSERT INTO student (FirstName,LastName,Phone)
-VALUES ('mohamed', 'family3', 123)  ,  ('mohamed', 'family3', 123);
 
+
+  
 -- Insert cours Data
-INSERT INTO cours (Name)
-VALUES ('MultiMedia')  ,  ('BDDA');
 
+
+  
 -- Insert coursTeacher
-INSERT INTO coursTeacher (IdTeacher,IdStudent,IdCour,DayWeek,StartHour)
-VALUES (1, 1, 1, 'Thursday', 8)  ,  (1, 2, 1, 'Thursday', 8)  ,  (2, 2, 2, 'Thursday', 9);
+
+
