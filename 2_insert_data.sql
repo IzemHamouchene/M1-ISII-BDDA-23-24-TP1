7b2392c28bb220959cab469332452504609c2732
IzemHamouchene <77283279+IzemHamouchene@users.noreply.github.com>
2024-04-19 10:10:19 +0100

    Update 2_insert_data.sql

--- 2_insert_data.sql
+++ 2_insert_data.sql
@@ -1,7 +1,7 @@
 USE UniversityDB
-GO
 -- Insert teacher Data
-
+INSERT INTO teacher (FirstName,LastName,Phone,Email)
+VALUES ('sofian' , 'family1' , 123 , 's_f3@gmail.com')  ,  ('yacin' , 'family2' , 456 , 'y_ f4@gmail.com');
 
   
 -- Insert student Data
